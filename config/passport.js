a5b5f85a56c60af8c175aff647722cf42490b045
Fabio Pacheco <fabiolins@live.com>
2018-02-21 21:17:56 -0300

    First commit

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+var config = require('./config')();
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
 var mongoose = require('mongoose');
@@ -7,8 +8,8 @@ module.exports = function() {
     var Usuario = mongoose.model('Usuario');
 
     passport.use(new GitHubStrategy({
-        clientID: 'da6289cfe7099e5338a6',
-        clientSecret: '00f3e7486839b9f64200b56bad1568145fdc0793',
+        clientID: config.clientID,
+        clientSecret: config.clientSecret,
         callbackURL: 'http://localhost:3000/auth/github/callback'   
     }, function(acessToken, refreshToken, profile, done) {
 
@@ -17,7 +18,6 @@ module.exports = function() {
             {"nome": profile.username},
             function(erro, usuario) {
                 if(erro) {
-                    console.log(erro);
                     return done(erro);
                 }
                 return done(null, usuario);
